62dca8076e9124a8f4424491e49bdba8d69d04cc
umer ghori <umerfayyaz500@gmail.com>
2025-12-31 11:35:33 +0500

    code

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -1,47 +1,43 @@
 <?php
 
-  
-
 namespace App\Imports;
 
-  
-
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
-use Hash;
 use Maatwebsite\Excel\Concerns\WithUpserts;
-  
+use Maatwebsite\Excel\Concerns\WithChunkReading;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
-class UsersImport implements ToModel, WithHeadingRow, WithUpserts
-
+class UsersImport implements ToModel, WithHeadingRow, WithUpserts, WithChunkReading
 {
-
     /**
-
-    * @param array $row
-
-    *
-
-    * @return \Illuminate\Database\Eloquent\Model|null
-
-    */
-
+     * @param array $row
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     public function model(array $row)
     {
-        
         return new User([
-
             'name'     => $row['name'],
-            'email'    => $row['email'], 
+            'email'    => $row['email'],
             'password' => Hash::make('password'),
-
         ]);
     }
-    
+
+    /**
+     * Column used for upsert (avoid duplicate entries)
+     */
     public function uniqueBy()
     {
         return 'email';
     }
 
+    /**
+     * Number of rows processed at a time
+     */
+    public function chunkSize(): int
+    {
+        return 10; 
+    }
 }
---
+++ app/Jobs/ExcelImportUserJob.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use App\imports\UsersImport;
+use Maatwebsite\Excel\Facades\Excel;
+use Illuminate\Support\Facades\Storage;
+
+class ExcelImportUserJob implements ShouldQueue
+{
+    use Queueable;
+
+    protected $filepath;
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($filepath)
+    {
+        $this->filepath = $filepath;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Excel::import(new UsersImport,Storage::disk('local')->path($this->filepath));
+
+        Storage::disk('local')->delete($this->filepath);
+    }
+}
