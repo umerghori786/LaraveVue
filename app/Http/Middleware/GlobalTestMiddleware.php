926c7519f44e3aa4254a445a199c34a112453552
umer ghori <umerfayyaz500@gmail.com>
2026-01-14 11:41:57 +0500

    index on cloumn

--- app/Http/Middleware/GlobalTestMiddleware.php
+++ app/Http/Middleware/GlobalTestMiddleware.php
@@ -5,6 +5,9 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use App\Models\Post;
+use DB;
+use Illuminate\Support\Benchmark;
 
 class GlobalTestMiddleware
 {
@@ -15,7 +18,55 @@ class GlobalTestMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {   
-        dump('helo thiss comes from GlobalTestMiddleware');
+        $data = Post::select([
+
+                    'user_id',
+                     DB::raw("COUNT('id') as total_posts")
+                ])
+                
+                ->with('user:id,name')
+                ->whereYear('created_at',2026)
+                ->groupBy('user_id')
+                ->get();
+        
+        Benchmark::dd([
+            'Scenario 1' => fn () => Post::select([
+
+                    'user_id',
+                     DB::raw("COUNT('id') as total_posts")
+                ])
+                
+                ->with('user:id,name')
+                ->whereYear('created_at',2026)
+                ->where('user_id',1)
+                ->groupBy('user_id')
+                ->get(), // 0.5 ms
+
+            'Scenario 2' => fn () => Post::select([
+
+                    'user_id',
+                     DB::raw("COUNT('id') as total_posts")
+                ])
+                
+                ->with('user:id,name')
+                ->whereYear('created_at',2026)
+                ->where('user_id',3)
+                ->groupBy('user_id')
+                ->lazy(), // 20.0 ms
+
+            'Scenario 3' => fn () => Post::select([
+
+                    'user_id',
+                     DB::raw("COUNT('id') as total_posts")
+                ])
+                
+                ->with('user:id,name')
+                ->whereYear('created_at',2026)
+                ->where('user_id',3)
+                ->groupBy('user_id')
+                ->cursor(), // 20.0 ms    
+        ]);  
+        //dump('helo thiss comes from GlobalTestMiddleware');
 
         return $next($request);
     }
